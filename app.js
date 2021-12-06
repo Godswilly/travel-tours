53ebd8e9b7dc78508125dd7c2fa19398429009fe
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-12-06 17:58:36 +0100

    Add tours POST request

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
@@ -15,6 +17,22 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({id: newId}, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      })
+    }
+  );
 });
 
 
