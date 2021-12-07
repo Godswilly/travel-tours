ff734c69ffaea7e6971a8777fe51e1f43a6e2bc8
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-12-07 19:02:09 +0100

    Create and mount multiple routers

--- app.js
+++ app.js
@@ -135,24 +135,31 @@ const deleteUser = (req, res) => {
   });
 };
 
+// ROUTES
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-.route('/api/v1/tours')
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+tourRouter
+.route('/')
 .get(getAllTours)
 .post(createTour);
 
-app
-.route('/api/v1/tours/:id')
+tourRouter
+.route('/:id')
 .get(getTour)
 .patch(updateTour)
 .delete(deleteTour);
 
-app
-.route('/api/v1/users')
+userRouter
+.route('/')
 .get(getAllUsers)
 .post(createUser);
 
-app.route('/api/v1/users/:id')
+userRouter
+.route('/:id')
 .get(getUser)
 .patch(updateUser)
 .delete(deleteUser);
