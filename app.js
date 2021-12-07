cda35c5ead00f1bc8f614a49d46c5f6f56a1238b
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-12-07 16:28:03 +0100

    Create custom middlewares

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
