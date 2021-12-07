0bd0102b9f0c8357c7ace9bc1fcde677f17aedcb
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-12-07 14:47:48 +0100

    Refactor routes

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -17,9 +16,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   })
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find(el => el.id === id);
@@ -37,9 +36,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour
     }
   })
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1
   const newTour = Object.assign({id: newId}, req.body);
 
@@ -53,9 +52,9 @@ app.post('/api/v1/tours', (req, res) => {
       })
     }
   );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -69,9 +68,8 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Upadated tour here...>'
     }
   });
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
+};
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -83,7 +81,25 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null
   });
-});
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app
+.route('/api/v1/tours')
+.get(getAllTours)
+.post(createTour);
+
+app
+.route('/api/v1/tours/:id')
+.get(getTour)
+.patch(updateTour)
+.delete(deleteTour);
+
 
 
 const port = 5000;
