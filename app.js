002821797bd25dae0e0b9cd68d4634a1f1e7172e
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-12-08 10:20:38 +0100

    Set environment variables

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 5000;
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
