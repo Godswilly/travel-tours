9991c67c2d3aaa1ba85c7ca6cbfbbced109d3653
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-13 06:34:47 +0100

    Connect database with express app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!!'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
