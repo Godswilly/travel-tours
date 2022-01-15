754fdaf890fb1e46422900c4d0a564a2468c9a2d
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-15 18:35:47 +0100

    Aggregation pipeline unwinding and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,7 +9,8 @@ router
   .route('/Top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours)
 
-router.route('/tour-stats').get(tourController.getTourStats);  
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);  
 
 router
   .route('/')
