ffe8fde3b0ed1838323f15c7f3f8914f02573fde
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-15 09:39:28 +0100

    Implement limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,6 +30,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt')
     }
 
+    // (3) FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     const tours = await query;
 
     // const query = await Tour.find()
