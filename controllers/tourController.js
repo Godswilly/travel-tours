f399b756e2781d8d904cbd79e1a9c7b2e16bab3c
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-15 11:47:36 +0100

    Implement pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // Create a shallow copy of the request.query object (BUILD QUERY)
 
     // (1A) FILTERING
@@ -39,6 +40,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    // EXECUTE QUERY
     const tours = await query;
 
     // const query = await Tour.find()
