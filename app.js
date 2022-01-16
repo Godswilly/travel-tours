4cdc3e63460090e8e2f891053c357c8981ca5aa0
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-16 19:25:31 +0100

    Check errors for unhandled routes

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -25,5 +20,12 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  })
+})
 
 module.exports = app;
