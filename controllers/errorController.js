fa5f1658a08ea7a743dd11d385b1a3b91bd4f5f3
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-17 17:38:38 +0100

    Handle mongoose errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -13,6 +13,13 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 }
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -56,6 +63,8 @@ module.exports = (err, req, res, next) => {
 
     if (err.name === 'CastError') error = handleCastErrorDB(err);
     if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+
     sendErrorProd(error, res);
   }
 }
