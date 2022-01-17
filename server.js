dc24aaf9116fa8abf6224de0b63dc1545f8ac9fd
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-17 18:21:03 +0100

    Catch uncaught exceptions

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', err => {
+  console.log('Uncaught Exception! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
@@ -14,6 +21,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
+  console.log('Unhandled Rejection! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
