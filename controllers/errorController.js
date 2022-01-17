b04ab6c19586b824598ace81503e237f6fb6b63a
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-17 17:03:20 +0100

    Handle duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,14 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 }
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value`;
+
+  return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -46,7 +54,8 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = {...err};
 
-    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
     sendErrorProd(error, res);
   }
 }
