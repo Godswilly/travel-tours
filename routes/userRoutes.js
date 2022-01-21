2c4be42115ba1d2f6a62249a652599f7229b139b
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-21 19:00:13 +0100

    Update current user data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protectRoute, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
