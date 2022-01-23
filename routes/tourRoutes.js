ea4dca2d099323eb0580f6c278dda3a23be2056d
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-23 17:32:09 +0100

    Implement simple nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -26,6 +27,14 @@ router
     authController.protectRoute,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protectRoute,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
