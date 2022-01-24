66c4acf0c58db34898a2213bbfaddb82207611ac
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-24 07:33:03 +0100

    Implement nested routes with express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/Top-5-cheap')
@@ -27,14 +29,6 @@ router
     authController.protectRoute,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protectRoute,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
