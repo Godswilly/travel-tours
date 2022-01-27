c2dd4a9d851a53296cf61058d6aa25dad32f6c15
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-27 15:54:01 +0100

    Create base template

--- app.js
+++ app.js
@@ -73,7 +73,10 @@ app.use((req, res, next) => {
 // ROUTES
 // view route
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Kalu',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
