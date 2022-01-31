47ff77f2dac2620704327f398145956720a2b4c0
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-01-31 21:32:28 +0100

    Build the tour overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour from data collection
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
