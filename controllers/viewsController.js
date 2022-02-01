ea4dc477438eaa25890af67eee2f6dac9efaf134
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-02-01 18:35:48 +0100

    Build the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,8 +11,15 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data, for the requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
+    tour,
   });
-};
+});
