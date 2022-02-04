c8d74ef7d12a3bf57abab3f47b6d55333a3829bb
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-02-04 19:35:12 +0100

    Build the login screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -23,3 +23,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
